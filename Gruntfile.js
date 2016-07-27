b4151e936279db604a84834e9cd0a3a8ae771277
Franco Sarachu <fsarachu@gmail.com>
2016-07-26 23:04:09 -0300

    Renamed "components" folder to "src" and added Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+
+  // Load Tasks
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Config Tasks
+  grunt.initConfig({
+    "concat": {
+      "scripts": {
+        "dist": {
+          "src": ['src/js/*.js'],
+          "dest": 'build/js/script.js'
+        }
+      }
+    },
+
+    "uglify": {
+      "target": {
+        "files": {
+          "build/js/script.min.js": ["build/js/script.js"]
+        }
+      }
+    }
+  });
+
+  // Register Tasks
+  grunt.registerTask();
+
+};
